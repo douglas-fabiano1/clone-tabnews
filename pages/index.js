876fa8b2dbd423b43034ea8909403ac270912710
Douglas Fabiano <119501911+douglas-fabiano1@users.noreply.github.com>
2024-11-28 23:10:47 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 function Home() {
-    return <h1>Amor,<br />
-        Essa mensagem é apenas<br />
-        para dizer o quanto eu sou feliz<br />
-        por ter você ao meu lado.<br />
-        Eu te amo hoje e sempre!<br />
+  return (
+    <h1>
+      Amor, essa mensagem é apenas para dizer o quanto eu sou feliz por ter você
+      ao meu lado. Eu te amo hoje e sempre!
     </h1>
+  );
 }
 
 export default Home;
