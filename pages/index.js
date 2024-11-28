ae25d679421d45d4a39831724dda5b714804fcd0
Douglas Fabiano <119501911+douglas-fabiano1@users.noreply.github.com>
2024-11-28 19:47:45 +0000

    Adiciona a página inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return <h1>Amor,<br />
+        Essa mensagem é apenas<br />
+        para dizer o quanto eu sou feliz<br />
+        por ter você ao meu lado.<br />
+        Eu te amo hoje e sempre!<br />
+    </h1>
+}
+
+export default Home;
